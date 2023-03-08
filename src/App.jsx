e6c826ba097bfcf7cc9ddfbb125e0818261a2177
Fezalion <kerimgileren@gmail.com>
2023-03-08 15:40:53 +0300

    scroll bar and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer.jsx"
 
 function App() {
   return (
-    <div className="relative h-auto bg-gradient-to-br from-primary-600 via-primary-700 to-primary-800">
+    <div className="relative h-auto bg-gradient-to-br from-primary-600 via-primary-700 to-primary-800 ">
       <Navbar />      
       <Home />      
       <Footer />
