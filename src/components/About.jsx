82872c674e362f7477696a132c8991a99f141f7c
Fezalion <kerimgileren@gmail.com>
2023-03-09 13:01:44 +0300

    About anim fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,12 @@ function About() {
   }, [controls, inView]);
   return (
     <>
-      <section className="z-0 h-auto max-w-screen-xl px-4 mx-auto text-center lg:min-h-screen lg:pb-8 lg:py-16 lg:px-12">
+      <motion.section className="z-0 h-auto max-w-screen-xl px-4 mx-auto text-center lg:min-h-screen lg:pb-8 lg:py-16 lg:px-12"
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={animationVariants}
+      >
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <motion.div className="mr-auto place-self-center lg:col-span-7 text-left"
             ref={ref}
@@ -52,7 +57,7 @@ function About() {
             />
           </motion.div>
         </div>
-      </section>
+      </motion.section>
     </>
   );
 }
--- src/globals/Animations.jsx
+++ src/globals/Animations.jsx
@@ -5,6 +5,7 @@ const animationVariants = {
     hiddenleft: { transform: "translateX(-25%)", opacity: 0 },
     hiddenup: { transform: "translateY(-25%)", opacity: 0 },
     hiddendown: { transform: "translateY(25%)", opacity: 0 },
+    hidden: { opacity: 0 },
     visible: { transform: "translateX(0%)", opacity: 1, transition: { duration: 0.5, ease: [0.5, 1, 0.89, 1] }},    
 }
 
