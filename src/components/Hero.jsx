845d09ad076f4e2c1ca0cab9d62359556280362f
Fezalion <kerimgileren@gmail.com>
2023-03-10 18:41:36 +0300

    new bg, footer adjust, lint

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,13 +22,15 @@ const container = {
     transition: {
       staggerChildren: 0.1,
       when: "beforeChildren",
+      ease: [0.5, 1, 0.89, 1],
+      duration: 0.5,
     },
   },
 };
 
 const item = {
-  hidden: { opacity: 0},
-  show: { opacity: 1, transition: { duration: 0.5, ease: [0.5, 1, 0.89, 1] } },
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
 };
 
 function Hero() {
@@ -57,14 +59,30 @@ function Hero() {
               initial="hidden"
               animate="show"
             >
-              <motion.span className="transform-gpu" variants={item}>F</motion.span>
-              <motion.span className="transform-gpu" variants={item}>e</motion.span>
-              <motion.span className="transform-gpu" variants={item}>z</motion.span>
-              <motion.span className="transform-gpu" variants={item}>a</motion.span>
-              <motion.span className="transform-gpu" variants={item}>l</motion.span>
-              <motion.span className="transform-gpu" variants={item}>i</motion.span>
-              <motion.span className="transform-gpu" variants={item}>o</motion.span>
-              <motion.span className="transform-gpu" variants={item}>n</motion.span>
+              <motion.span className="transform-gpu" variants={item}>
+                F
+              </motion.span>
+              <motion.span className="transform-gpu" variants={item}>
+                e
+              </motion.span>
+              <motion.span className="transform-gpu" variants={item}>
+                z
+              </motion.span>
+              <motion.span className="transform-gpu" variants={item}>
+                a
+              </motion.span>
+              <motion.span className="transform-gpu" variants={item}>
+                l
+              </motion.span>
+              <motion.span className="transform-gpu" variants={item}>
+                i
+              </motion.span>
+              <motion.span className="transform-gpu" variants={item}>
+                o
+              </motion.span>
+              <motion.span className="transform-gpu" variants={item}>
+                n
+              </motion.span>
             </motion.span>
           </motion.h1>
           <motion.p
