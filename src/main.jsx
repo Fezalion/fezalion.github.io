0e527d6f20fb73fee428a4871459fb008b3938ad
Fezalion <kerimgileren@gmail.com>
2023-03-10 13:23:43 +0300

    switch back to browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 import Layout from "./Layout";
@@ -10,15 +10,15 @@ import NoPage from "./pages/NoPage";
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />          
-        </Route>
-        <Route path="*" element={<NoPage />} />
+          <Route path="about" element={<About />} />  
+          <Route path="*" element={<NoPage />} />        
+        </Route>        
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
