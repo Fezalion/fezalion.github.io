8ef6828bc156f482eca604d7b4f99abb5265d344
Fezalion <kerimgileren@gmail.com>
2023-03-14 19:54:18 +0300

    about remake time

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,91 +1,25 @@
-import React, { useEffect } from "react";
-import { useAnimation, motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+import React from "react";
+import { motion } from "framer-motion";
 
-const container = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.15,
-      when: "beforeChildren",
-    },
-  },
-};
-
-const item = {
-  hidden: { opacity: 0 },
-  show: { opacity: 1 },
-};
+const Dict = [
+  ["test", "test"],
+  ["test2", "test2"],
+  ["test3", "test3"],
+  ["test4", "test4"],
+];
 
 function About() {
-  const controls = useAnimation();
-  const [ref, inView] = useInView();
-  useEffect(() => {
-    if (inView) {
-      controls.start("show");
-    }
-  }, [controls, inView]);
   return (
     <>
       <motion.section
-        className="relative z-0 grid content-center w-auto h-auto min-w-full min-h-screen px-4 mx-auto text-center lg:min-h-screen lg:pb-8 lg:py-16 lg:px-12"
+        className="relative z-0 grid content-center w-auto h-auto min-w-full min-h-screen mx-auto text-center snap-center"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
       >
-        <motion.article
-          className="items-center max-w-screen-xl gap-8 px-4 py-8 mx-auto xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6 snap-center"
-          ref={ref}
-          initial="hidden"
-          animate={controls}
-          variants={container}
-        >
-          <motion.img
-            className="block w-full"
-            src="https://placehold.jp/990099/000000/300x300.png"
-            alt="dashboard image"
-            variants={item}
-          />
-          <motion.div className="mt-4 md:mt-0" variants={item}>
-            <motion.h2 className="mb-4 text-4xl font-extrabold tracking-tight text-left text-primary-100">
-              title
-            </motion.h2>
-            <motion.p className="mb-6 font-light text-left text-gray-500 md:text-lg">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem tenetur quaerat temporibus reiciendis dolore
-              aperiam eveniet vitae, nesciunt distinctio provident. Explicabo
-              modi recusandae nobis autem exercitationem eveniet obcaecati ab.
-              Magni.
-            </motion.p>
-          </motion.div>
-        </motion.article>
-        <motion.article
-          className="items-center max-w-screen-xl gap-8 px-4 py-8 mx-auto xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6 snap-center"
-          ref={ref}
-          initial="hidden"
-          animate={controls}
-          variants={container}
-        >
-          <motion.div className="mt-4 md:mt-0" variants={item}>
-            <motion.h2 className="mb-4 text-4xl font-extrabold tracking-tight text-right text-primary-100">
-              title
-            </motion.h2>
-            <motion.p className="mb-6 font-light text-right text-gray-500 md:text-lg">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Exercitationem tenetur quaerat temporibus reiciendis dolore
-              aperiam eveniet vitae, nesciunt distinctio provident. Explicabo
-              modi recusandae nobis autem exercitationem eveniet obcaecati ab.
-              Magni.
-            </motion.p>
-          </motion.div>
-          <motion.img
-            className="block w-full"
-            src="https://placehold.jp/990099/000000/300x300.png"
-            alt="dashboard image"
-            variants={item}
-          />
-        </motion.article>
+        <motion.div>
+          
+        </motion.div>
       </motion.section>
     </>
   );
