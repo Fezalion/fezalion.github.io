4171e9a2a0eb23bc6dc4d1cc1b1ee5dbf209dfca
Fezalion <kerimgileren@gmail.com>
2023-03-15 18:19:37 +0300

    fix animation re-rendering on layout changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,38 +1,11 @@
-import React, { Suspense, useEffect } from "react";
+import React, { Suspense } from "react";
 import { Triangle } from "react-loader-spinner";
 import { Outlet } from "react-router-dom";
 
 const Navbar = React.lazy(() => import("./components/Navbar.jsx"));
 const Footer = React.lazy(() => import("./components/Footer.jsx"));
 
-function Layout() {
-  function lerp(start, end, amt) {
-    return (1 - amt) * start + amt * end;
-  }
-
-  useEffect(() => {
-    var clientX = 1024, clientY = 0;
-    var newX = 1024, newY = 0;
-    const updateMousePos = (ev) => {
-      clientX = ev.clientX;
-      clientY = ev.clientY;
-    };
-
-    var interval = setInterval(() => {
-      newX = lerp(newX, clientX, 0.2);
-      newY = lerp(newY, clientY, 0.2);
-      document.documentElement.style.setProperty("--x", `${newX}px`);
-      document.documentElement.style.setProperty("--y", `${newY}px`);
-    }, 1000 / 30);
-
-    window.addEventListener("mousemove", updateMousePos);
-
-    return () => {
-      window.removeEventListener("mousemove", updateMousePos);
-      clearInterval(interval);
-    };
-  }, []);
-
+function Layout() {  
   return (
     <div className="relative h-auto min-h-screen">
       <Suspense
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
@@ -6,6 +6,32 @@ import "./index.css";
 import AnimatedRoutes from "./pages/AnimatedRoutes";
 
 export default function App() {
+  function lerp(start, end, amt) {
+    return (1 - amt) * start + amt * end;
+  }
+
+  useEffect(() => {
+    var clientX = 0, clientY = 0;
+    var newX = 0, newY = 0;
+    const updateMousePos = (ev) => {
+      clientX = ev.clientX;
+      clientY = ev.clientY;
+    };
+
+    var interval = setInterval(() => {
+      newX = lerp(newX, clientX, 0.2);
+      newY = lerp(newY, clientY, 0.2);
+      document.documentElement.style.setProperty("--x", `${newX}px`);
+      document.documentElement.style.setProperty("--y", `${newY}px`);
+    }, 1000 / 30);
+
+    window.addEventListener("mousemove", updateMousePos);
+
+    return () => {
+      window.removeEventListener("mousemove", updateMousePos);
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <BrowserRouter>
       <AnimatedRoutes />
