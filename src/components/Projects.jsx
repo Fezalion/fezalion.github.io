2205dae413725de811fc5b2e7dede5e8bc5ab0db
Fezalion <kerimgileren@gmail.com>
2023-03-20 03:49:41 +0300

    code breakup for projects and html parsing for links in json

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import parse from "html-react-parser";
 import { domAnimation, LazyMotion, m } from "framer-motion";
+import myprojects from "./data/projects";
 
 const container = {
   hidden: { opacity: 0 },
@@ -49,184 +51,32 @@ function Projects() {
             initial="hidden"
             animate="show"
           >
-            <m.article variants={item}>
-              <h3 className="mb-2 w-auto text-xl font-bold text-primary-100">
-                <span className="relative">
-                  FezBotRedux
-                  <a
-                    className="absolute top-1 -right-6 scale-75"
-                    target="_blank"
-                    href="https://github.com/Fezalion/FezBotRedux"
-                  >
-                    <svg
-                      fill="currentColor"
-                      width="24"
-                      height="24"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fillRule="evenodd"
-                      clipRule="evenodd"
+            {myprojects.map((project, i) => (
+              <m.article key={i}>
+                <h3 className="mb-2 w-auto text-xl font-bold text-primary-100">
+                  <span className="relative">
+                    {project.name}
+                    <a
+                      className="absolute top-1 -right-6 scale-75"
+                      target="_blank"
+                      href={project.link}
                     >
-                      <path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z" />
-                    </svg>
-                  </a>
-                </span>
-              </h3>
-              <p className="text-gray-400">
-                A discord chat bot coded in{" "}
-                <a
-                  className="cursor-pointer text-secondary-500"
-                  target="_blank"
-                  href="https://discordnet.dev/"
-                >
-                  Discord.NET
-                </a>{" "}
-                library, mainly used privately in my own server.
-              </p>
-            </m.article>
-            <m.article variants={item}>
-              <h3 className="mb-2 w-auto text-xl font-bold text-primary-100">
-                <span className="relative">
-                  ConsoleMenuGeneric
-                  <a
-                    className="absolute top-1 -right-6 scale-75"
-                    target="_blank"
-                    href="https://github.com/Fezalion/ConsoleMenuGeneric"
-                  >
-                    <svg
-                      fill="currentColor"
-                      width="24"
-                      height="24"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                    >
-                      <path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z" />
-                    </svg>
-                  </a>
-                </span>
-              </h3>
-              <p className="text-gray-400">
-                Keyboard operated switch menu coded in C#, mainly used for
-                personal highschool projects and abandoned since graduation.
-              </p>
-            </m.article>
-            <m.article variants={item}>
-              <h3 className="mb-2 w-auto text-xl font-bold text-primary-100">
-                <span className="relative">
-                  ForzaTuneHelper
-                  <a
-                    className="absolute top-1 -right-6 scale-75"
-                    target="_blank"
-                    href="https://github.com/Fezalion/ForzaTuneHelper"
-                  >
-                    <svg
-                      fill="currentColor"
-                      width="24"
-                      height="24"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                    >
-                      <path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z" />
-                    </svg>
-                  </a>
-                </span>
-              </h3>
-              <p className="text-gray-400">
-                I love racing games. Something I love more than racing games is
-                drifting in racing games. I hate tuning cars for hours, so I
-                made a tool to help me tune my cars to drift. This was made for{" "}
-                <a
-                  className="cursor-pointer text-secondary-500"
-                  target="_blank"
-                  href="https://store.steampowered.com/app/1293830/Forza_Horizon_4/"
-                >
-                  Forza Horizon 4
-                </a>{" "}
-                but abandoned since newer titles came out.
-              </p>
-            </m.article>
-            <m.article variants={item}>
-              <h3 className="mb-2 w-auto text-xl font-bold text-primary-100">
-                <span className="relative">
-                  Fezalion.tech
-                  <a
-                    className="absolute top-1 -right-6 scale-75"
-                    target="_blank"
-                    href="https://github.com/Fezalion/Fezalion.tech"
-                  >
-                    <svg
-                      fill="currentColor"
-                      width="24"
-                      height="24"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                    >
-                      <path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z" />
-                    </svg>
-                  </a>
-                </span>
-              </h3>
-              <p className="text-gray-400">
-                This very website you are on, built with ReactJS, using vite.
-              </p>
-            </m.article>
-            <m.article variants={item}>
-              <h3 className="mb-2 w-auto text-xl font-bold text-primary-100">
-                <span className="relative">
-                  thefuck by nvbn
-                  <a
-                    className="absolute top-1 -right-6 scale-75"
-                    target="_blank"
-                    href="https://github.com/nvbn/thefuck"
-                  >
-                    <svg
-                      fill="currentColor"
-                      width="24"
-                      height="24"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                    >
-                      <path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z" />
-                    </svg>
-                  </a>
-                </span>
-              </h3>
-              <p className="text-gray-400">
-                Magnificent app which corrects your previous console command,
-                which is something I can't live without anymore. Its genious
-                work.
-              </p>
-            </m.article>
-            <m.article variants={item}>
-              <h3 className="mb-2 w-auto text-xl font-bold text-primary-100">
-                <span className="relative">
-                  owoifier
-                  <a
-                    className="absolute top-1 -right-6 scale-75"
-                    target="_blank"
-                    href="https://github.com/powercord-community/owoifier"
-                  >
-                    <svg
-                      fill="currentColor"
-                      width="24"
-                      height="24"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                    >
-                      <path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z" />
-                    </svg>
-                  </a>
-                </span>
-              </h3>
-              <p className="text-gray-400">
-                Dead project of mine, after powercord closed I stopped
-                developing it.
-              </p>
-            </m.article>
+                      <svg
+                        fill="currentColor"
+                        width="24"
+                        height="24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                      >
+                        <path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z" />
+                      </svg>
+                    </a>
+                  </span>
+                </h3>
+                <p className="text-gray-400">{parse(project.desc)}</p>
+              </m.article>
+            ))}
           </m.div>
         </div>
       </m.section>
---
+++ src/components/data/projects.js
@@ -0,0 +1,34 @@
+const myprojects = [
+  {
+    name: "FezBotRedux",
+    desc: 'A discord chat bot coded in <a className="cursor-pointer text-secondary-500" target="_blank" href="https://discordnet.dev/">Discord.NET</a> library, mainly used privately in my own server.',
+    link: "https://github.com/Fezalion/FezBotRedux",
+  },
+  {
+    name: "ConsoleMenuGeneric",
+    desc: "Keyboard operated switch menu coded in C#, mainly used for personal highschool projects and abandoned since graduation.",
+    link: "https://github.com/Fezalion/ConsoleMenuGeneric",
+  },
+  {
+    name: "ForzaTuneHelper",
+    desc: 'I love racing games. Something I love more than racing games is drifting in racing games. I hate tuning cars for hours, so I made a tool to help me tune my cars to drift. This was made for <a className="cursor-pointer text-secondary-500" target="_blank" href="https://store.steampowered.com/app/1293830/Forza_Horizon_4/">Forza Horizon 4</a> but abandoned since newer titles came out.',
+    link: "https://github.com/Fezalion/ForzaTuneHelper",
+  },
+  {
+    name: "Fezalion.tech",
+    desc: "This very website you are on, built with ReactJS, using vite.",
+    link: "https://github.com/Fezalion/Fezalion.tech",
+  },
+  {
+    name: "thefuck by nvbn",
+    desc: "Magnificent app which corrects your previous console command, which is something I can't live without anymore. Its genious work.",
+    link: "https://github.com/nvbn/thefuck",
+  },
+  {
+    name: "owoifier",
+    desc: "Dead project of mine, after powercord got decontinued I stopped developing it.",
+    link: "https://github.com/powercord-community/owoifier",
+  },
+];
+
+export default myprojects;
