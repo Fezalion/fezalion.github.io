2af6a1c97a54f3b66950c233b140f530520e1a19
Fezalion <kerimgileren@gmail.com>
2023-07-11 05:56:31 +0300

    uhm card test

--- src/components/Gallery/images.jsx
+++ src/components/Gallery/images.jsx
@@ -11,11 +11,47 @@ import img5 from "../../images/00004.png";
 const images = [img1, img2, img3, img4, img5];
 
 const ImagesComponent = () => {
+
+  function map(val, minA, maxA, minB, maxB) {
+    return minB + ((val - minA) * (maxB - minB)) / (maxA - minA);
+  }
+  
+  function Card3D(card, ev) {
+    let img = card.querySelector('img');
+    let imgRect = card.getBoundingClientRect();
+    let width = imgRect.width;
+    let height = imgRect.height;
+    let mouseX = ev.offsetX;
+    let mouseY = ev.offsetY;
+    let rotateY = map(mouseX, 0, 180, -25, 25);
+    let rotateX = map(mouseY, 0, 250, 25, -25);
+    let brightness = map(mouseY, 0, 250, 1.5, 0.5);
+    
+    img.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    img.style.filter = `brightness(${brightness})`;
+  }
+  
+  var cards = document.querySelectorAll('.card3d');
+  
+  cards.forEach((card) => {
+    card.addEventListener('mousemove', (ev) => {
+      Card3D(card, ev);
+    });
+    
+    card.addEventListener('mouseleave', (ev) => {
+      let img = card.querySelector('img');
+      
+      img.style.transform = 'rotateX(0deg) rotateY(0deg)';
+      img.style.filter = 'brightness(1)';
+    });
+  });
+
+
   return (
     <>
       {images.map((img, i) => (
         <m.div
-          className="flex h-full w-full flex-col items-center justify-end gap-4 rounded border-secondary-500 py-4 px-2 backdrop-blur-sm lg:border"
+          className="card3d flex h-full w-full flex-col items-center justify-end gap-4 rounded border-secondary-500 py-4 px-2 backdrop-blur-sm lg:border"
           variants={FezAnimations.container}
           key={i}
         >
